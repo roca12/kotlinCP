f01d2a607bb76a496de3b29d923db5c9ddd1e54e
roca12 <roca230313@gmail.com>
2021-02-11 15:49:29 -0500

    Bigint

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,15 @@
+import java.math.BigInteger
 import java.util.Scanner
 
-fun sumar(a: Int, b: Int): Int {
-    return a + b
-}
 fun main(args: Array<String>) {
     val sc=Scanner(System.`in`)
-    var a=sc.nextInt().toInt()
-    for(num in 1..a){
-        var b=sc.nextInt().toInt()
-        var c=sc.nextInt().toInt()
-        var resultado = sumar(b,c)
-        println("$resultado")
+    var casos=sc.nextInt().toInt()
+    sc.nextLine()
+    for (i in 0 until casos) {
+        val entrada=sc.nextLine()
+        val num1 = BigInteger(entrada)
+        val num2 = BigInteger.valueOf(2)
+        val res=(num1/num2)+num2
+        print(res)
     }
 }
